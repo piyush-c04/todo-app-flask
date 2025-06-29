6d5e3a31789b6935047eae2b44282eb701cb91c3
piyush-c04 <143525614+piyush-c04@users.noreply.github.com>
2025-06-29 13:27:13 +0530

    final commit of working model

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -13,6 +13,7 @@ def view_tasks():
     tasks = Task.query.all()
     return render_template('tasks.html', tasks=tasks)
 
+
 @tasks_bp.route('/add', methods=['POST'])
 def add_task():
     if 'user' not in session:
